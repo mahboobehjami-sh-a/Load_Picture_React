6f351d9a34bd868d9e806d35c1948c46043a69b2
mahboobehjami-sh-a <mahboobehjami1380@gmail.com>
2022-12-25 16:22:50 +0330

    The photo was displayed and the project was completed😍

--- src/App.js
+++ src/App.js
@@ -3,23 +3,52 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, Card, Row } from 'react-bootstrap'
 import { useState, useEffect } from 'react'
 
-
 function App() {
+
+  const [picture, setPicture] = useState([])
+  useEffect(() => {
+
+    var headersapi = {
+      method: 'GET',
+      // mode: 'cors',
+      // credentials: 'include',
+      // withCredentials: true,
+      // crossorigin: true,
+      // mode: 'cors',
+      Headers: {
+        'Content-Type': 'application/json'
+        // 'Access-Control-Allow-Origin': '*',
+        // 'Access-Control-Allow-Credentials': true
+      },
+    }
+
+
+    const loadpicture = async () => {
+      var pictureData = await fetch('https://random-d.uk/api/random', headersapi)
+        .then(Response =>
+          Response.json())
+        .then(data => {
+          setPicture(data)
+        })
+    }
+    loadpicture()
+  })
+
+
   return (
     <div className="App">
-      <row>
-        <Card className='col-10 m-5'>
-          <Card.Img src="#">
-
-          </Card.Img>
-          <Card.Body>
-            <Card.Title>
-              title
-            </Card.Title>
-          </Card.Body>
-        </Card>
-
-      </row>
+      <Container>
+        <Row className="center">
+          <Card className='col-10 m-5 bg-info'>
+            <Card.Img className='p-5 ' src={picture.url} />
+            <Card.Body>
+              <Card.Title>
+                {picture.message}
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
     </div>
   );
 }
